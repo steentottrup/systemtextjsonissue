52068d0d7af5526bcda60abf6d50ce640e59f8fb
Steen F. Tøttrup <sft@creativeminds.dk>
2019-10-10 07:42:20 +0200

    initial commit

---
+++ systemtextjsonissue/Controllers/WeatherForecastController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace systemtextjsonissue.Controllers {
+	[ApiController]
+	[Route("[controller]")]
+	public class WeatherForecastController : ControllerBase {
+		private static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+
+		private readonly ILogger<WeatherForecastController> _logger;
+
+		public WeatherForecastController(ILogger<WeatherForecastController> logger) {
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public SurveyReportResult Get() {
+			return new SurveyReportResult {
+				Id = Guid.NewGuid(),
+				Name = "Root",
+				MemberCount = 4,
+				OrganisationalUnits = new OrganisationalUnit[] {
+					new OrganisationalUnitWithParticipantGroups {
+						Id = Guid.NewGuid(),
+						Name = "Level 2",
+						MemberCount = 4,
+						ParticipantGroups = new ParticipantGroup[]{
+							new ParticipantGroup {
+								Id = Guid.NewGuid(),
+								MemberCount = 4,
+								Name = "end"
+							}
+						}
+					}
+				}
+			};
+		}
+	}
+
+
+
+	public class SurveyReportResult {
+		public Guid Id { get; set; }
+		public String Name { get; set; }
+		public Int32 MemberCount { get; set; }
+		public IEnumerable<OrganisationalUnit> OrganisationalUnits { get; set; }
+	}
+
+	public abstract class OrganisationalUnit {
+		public Guid Id { get; set; }
+		public String Name { get; set; }
+		public Int32 MemberCount { get; set; }
+
+	}
+
+	public class OrganisationalUnitWithParticipantGroups : OrganisationalUnit {
+		public IEnumerable<ParticipantGroup> ParticipantGroups { get; set; }
+	}
+
+	public class ParticipantGroup {
+		public Guid Id { get; set; }
+		public String Name { get; set; }
+		public Int32 MemberCount { get; set; }
+	}
+
+}
